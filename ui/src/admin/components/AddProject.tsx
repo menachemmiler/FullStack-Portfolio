bfd1a149cf12c12d74c5ef952fb31b865c89548c
menachem miler <ms0556727820@gmail.com>
2025-07-09 16:22:59 +0300

    with staus axios

--- ui/src/admin/components/AddProject.tsx
+++ ui/src/admin/components/AddProject.tsx
@@ -1,138 +1,205 @@
 import { Button, TextField } from "@mui/material";
-import { IProjectForm } from "../../utils/interfaces";
+import { DataStatus, IProjectForm } from "../../utils/interfaces";
 import { SendIcon } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "../../redux/store";
+import loading from "../../assets/loading.gif";
+import { useEffect, useState } from "react";
+import {
+  addNewProject,
+  resetStatusNewProject,
+} from "../../redux/slices/projectSlice";
 
-interface AddProjectProps {
-  project: IProjectForm;
-  setProject: React.Dispatch<React.SetStateAction<IProjectForm>>;
-  handleSubmit: (e: React.FormEvent) => void;
-  imageError?: boolean;
-}
+const AddProject = () => {
+  const dispatch = useAppDispatch();
+  const { newProject, statusNewProject } = useAppSelector(
+    (state) => state.projects
+  );
+  const [project, setProject] = useState<IProjectForm>({
+    title: "",
+    description: "",
+    fullDescription: "",
+    image: new File([], "No File Chosen"),
+    liveLink: "",
+    backendLink: "",
+  });
+  const [imageError, setImageError] = useState(false);
 
-const AddProject = ({
-  project,
-  setProject,
-  handleSubmit,
-  imageError,
-}: AddProjectProps) => {
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!project.image.size) {
+      setImageError(true);
+      setTimeout(() => setImageError(false), 1000);
+      return;
+    }
+    try {
+      dispatch(addNewProject(project));
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    if (statusNewProject == DataStatus.FAILED) {
+      setTimeout(() => dispatch(resetStatusNewProject()), 2000);
+    } else if (statusNewProject == DataStatus.SUCCESS) {
+      setTimeout(() => dispatch(resetStatusNewProject()), 2000);
+      setProject({
+        title: "",
+        description: "",
+        fullDescription: "",
+        image: new File([], "placeholder.txt"),
+        liveLink: "",
+        backendLink: "",
+        githubAll: "",
+        githubClient: "",
+        githubServer: "",
+      });
+    }
+  }, [statusNewProject]);
+
   return (
     <div className="add-project">
       <div className="header">
         <h2>Add New Project</h2>
       </div>
       <form onSubmit={handleSubmit}>
-        <TextField
-          id="title"
-          label="Project Title"
-          value={project.title}
-          variant="filled"
-          onChange={(e) => setProject({ ...project, title: e.target.value })}
-          required
-          size="small"
-        />
-        <TextField
-          id="description"
-          label="Project Description"
-          value={project.description}
-          onChange={(e) =>
-            setProject({ ...project, description: e.target.value })
-          }
-          variant="filled"
-          required
-          size="small"
-        />
-        <TextField
-          label="Project Live Link"
-          id="liveLink"
-          value={project.liveLink}
-          onChange={(e) => setProject({ ...project, liveLink: e.target.value })}
-          required
-          variant="filled"
-          size="small"
-        />
-        <TextField
-          id="backendLink"
-          label="Backend Server Link"
-          value={project.backendLink}
-          variant="filled"
-          onChange={(e) =>
-            setProject({ ...project, backendLink: e.target.value })
-          }
-          size="small"
-        />
-        <TextField
-          id="fullDescription"
-          value={project.fullDescription}
-          label="Full Description"
-          multiline
-          rows={3}
-          variant="filled"
-          onChange={(e) =>
-            setProject({ ...project, fullDescription: e.target.value })
-          }
-          required
-          size="small"
-        />
-        <TextField
-          id="githubClient"
-          label="Github Client Link"
-          value={project.githubClient}
-          variant="filled"
-          onChange={(e) =>
-            setProject({ ...project, githubClient: e.target.value })
-          }
-          size="small"
-        />
-        <TextField
-          id="githubServer"
-          label="Github Server Link"
-          value={project.githubServer}
-          variant="filled"
-          onChange={(e) =>
-            setProject({ ...project, githubServer: e.target.value })
-          }
-          size="small"
-        />
-        <TextField
-          id="githubAll"
-          label="Github All Link"
-          value={project.githubAll}
-          variant="filled"
-          onChange={(e) =>
-            setProject({ ...project, githubAll: e.target.value })
-          }
-          size="small"
-        />
-        <div className="image">
-          <p
-            className={
-              project.image.size
-                ? ""
-                : imageError
-                ? "no-file-chosen error-blinking"
-                : "no-file-chosen"
-            }
-          >
-            {project.image.name}
-          </p>
-          <Button component="label" variant="outlined">
-            Project Image +
-            <input
-              hidden
-              type="file"
-              multiple
+        {statusNewProject === "LOADING" && (
+          <div className="loading">
+            <img src={loading} alt="Loading..." />
+          </div>
+        )}
+        {statusNewProject === "FAILED" && (
+          <div className="error">
+            Failed to create project, please try again.
+          </div>
+        )}
+        {statusNewProject === "SUCCESS" && (
+          <div className="success">
+            {`Project created successfully: ${newProject?.title}`}
+          </div>
+        )}
+        {statusNewProject === DataStatus.IDLE && (
+          <>
+            <TextField
+              id="title"
+              label="Project Title"
+              value={project.title}
+              variant="filled"
               onChange={(e) =>
-                setProject({
-                  ...project,
-                  image: e.target.files ? e.target.files[0] : new File([], ""),
-                })
+                setProject({ ...project, title: e.target.value })
               }
+              required
+              size="small"
             />
-          </Button>
-        </div>
-        <Button variant="contained" type="submit" endIcon={<SendIcon />}>
-          Save Project
-        </Button>
+            <TextField
+              id="description"
+              label="Project Description"
+              value={project.description}
+              onChange={(e) =>
+                setProject({ ...project, description: e.target.value })
+              }
+              variant="filled"
+              required
+              size="small"
+            />
+            <TextField
+              label="Project Live Link"
+              id="liveLink"
+              value={project.liveLink}
+              onChange={(e) =>
+                setProject({ ...project, liveLink: e.target.value })
+              }
+              required
+              variant="filled"
+              size="small"
+            />
+            <TextField
+              id="backendLink"
+              label="Backend Server Link"
+              value={project.backendLink}
+              variant="filled"
+              onChange={(e) =>
+                setProject({ ...project, backendLink: e.target.value })
+              }
+              size="small"
+            />
+            <TextField
+              id="fullDescription"
+              value={project.fullDescription}
+              label="Full Description"
+              multiline
+              rows={3}
+              variant="filled"
+              onChange={(e) =>
+                setProject({ ...project, fullDescription: e.target.value })
+              }
+              required
+              size="small"
+            />
+            <TextField
+              id="githubClient"
+              label="Github Client Link"
+              value={project.githubClient}
+              variant="filled"
+              onChange={(e) =>
+                setProject({ ...project, githubClient: e.target.value })
+              }
+              size="small"
+            />
+            <TextField
+              id="githubServer"
+              label="Github Server Link"
+              value={project.githubServer}
+              variant="filled"
+              onChange={(e) =>
+                setProject({ ...project, githubServer: e.target.value })
+              }
+              size="small"
+            />
+            <TextField
+              id="githubAll"
+              label="Github All Link"
+              value={project.githubAll}
+              variant="filled"
+              onChange={(e) =>
+                setProject({ ...project, githubAll: e.target.value })
+              }
+              size="small"
+            />
+            <div className="image">
+              <p
+                className={
+                  project.image.size
+                    ? ""
+                    : imageError
+                    ? "no-file-chosen error-blinking"
+                    : "no-file-chosen"
+                }
+              >
+                {project.image.name}
+              </p>
+              <Button component="label" variant="outlined">
+                Project Image +
+                <input
+                  hidden
+                  type="file"
+                  multiple
+                  onChange={(e) =>
+                    setProject({
+                      ...project,
+                      image: e.target.files
+                        ? e.target.files[0]
+                        : new File([], ""),
+                    })
+                  }
+                />
+              </Button>
+            </div>
+            <Button variant="contained" type="submit" endIcon={<SendIcon />}>
+              Save Project
+            </Button>
+          </>
+        )}
       </form>
     </div>
   );
--- ui/src/admin/components/projectManagement.tsx
+++ ui/src/admin/components/projectManagement.tsx
@@ -1,85 +1,31 @@
-import { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "../../redux/store";
-import { getProfile } from "../../redux/slices/userSlice";
-import { DataStatus, IProjectForm } from "../../utils/interfaces";
-import {
-  addNewProject,
-  resetStatusNewProject,
-} from "../../redux/slices/projectSlice";
+import { useAppSelector } from "../../redux/store";
+import { DataStatus } from "../../utils/interfaces";
 import EditProjects from "./editProjects";
 import AddProject from "./addProject";
+import loading from "../../assets/loading.gif";
 
 const ProjectManagement = () => {
-  const dispatch = useAppDispatch();
   const { error, status, user } = useAppSelector((state) => state.user);
-  const { newProject, statusNewProject } = useAppSelector(
-    (state) => state.projects
-  );
-  const [project, setProject] = useState<IProjectForm>({
-    title: "",
-    description: "",
-    fullDescription: "",
-    image: new File([], "No File Chosen"),
-    liveLink: "",
-    backendLink: "",
-  });
-  const [imageError, setImageError] = useState(false);
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!project.image.size) {
-      setImageError(true);
-      setTimeout(() => setImageError(false), 1000);
-      return;
-    }
-    try {
-      dispatch(addNewProject(project));
-    } catch (err: any) {
-      console.log(err.message);
-    }
-  };
-
-  useEffect(() => {
-    dispatch(getProfile());
-  }, []);
-
-  useEffect(() => {
-    if (statusNewProject == DataStatus.FAILED) {
-      alert("Can't create project, please try again");
-    } else if (statusNewProject == DataStatus.SUCCESS) {
-      alert(`Project created successfully: ${newProject?.title}`);
-      dispatch(resetStatusNewProject());
-      setProject({
-        title: "",
-        description: "",
-        fullDescription: "",
-        image: new File([], "placeholder.txt"),
-        liveLink: "",
-        backendLink: "",
-        githubAll: "",
-        githubClient: "",
-        githubServer: "",
-      });
-    }
-  }, [statusNewProject]);
-
-  if (status == DataStatus.LOADING) return <p>Loading...</p>;
-  if (error) return <p style={{ color: "red" }}>{error}</p>;
 
   if (user) {
     return (
       <div className="page">
+        {status === DataStatus.LOADING && (
+          <>
+            <img src={loading} alt="Loading..." />
+          </>
+        )}
+        {status === DataStatus.FAILED && (
+          <>
+            <p style={{ color: "red" }}>{error}</p>{" "}
+          </>
+        )}
         <div className="project-management">
           <div className="header">
             <h1>Hello {user.username}</h1>
           </div>
           <div className="main">
-            <AddProject
-              handleSubmit={handleSubmit}
-              project={project}
-              setProject={setProject}
-              imageError={imageError}
-            />
+            <AddProject />
             <EditProjects />
           </div>
         </div>
