24c63082d785e174dc355e85d9176547056dac0a
menachem miler <ms0556727820@gmail.com>
2025-07-09 15:03:59 +0300

    with loading gif

--- ui/src/components/Projects.tsx
+++ ui/src/components/Projects.tsx
@@ -2,24 +2,33 @@ import { useSelector } from "react-redux";
 import ProjectCard from "./ProjectCard";
 import { RootState } from "../redux/store";
 import { DataStatus } from "../utils/interfaces";
+import loading from "../assets/loading.gif";
 
 const Projects = () => {
   const { error, projects, status } = useSelector(
     (state: RootState) => state.projects
   );
 
-  if (status == DataStatus.LOADING) return <p>Loading...</p>;
-  if (error) return <p style={{ color: "red" }}>{error}</p>;
   return (
     <div className="page">
       <section className="projects-section">
-        <h2>My projects</h2>
-        <div className="projects-flex">
-          {projects &&
-            projects.map((project) => (
-              <ProjectCard key={project._id} project={project} />
-            ))}
-        </div>
+        {status === DataStatus.LOADING && (
+          <img src={loading} alt="Loading..." />
+        )}
+        {status === DataStatus.FAILED && (
+          <p style={{ color: "red" }}>{error}</p>
+        )}
+        {status === DataStatus.SUCCESS && (
+          <>
+            <h2>My projects</h2>
+            <div className="projects-flex">
+              {projects &&
+                projects.map((project) => (
+                  <ProjectCard key={project._id} project={project} />
+                ))}
+            </div>
+          </>
+        )}
       </section>
     </div>
   );
