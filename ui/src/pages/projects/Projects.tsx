de4b173d2f0de9247c2c13978196e6da80f8727a
miler.yesodot <miler.yesodot@gmail.com>
2025-12-03 16:13:33 +0200

    refactor: Enhance project and home pages with improved layout and styling; add hero content and social links

--- ui/src/pages/projects/Projects.tsx
+++ ui/src/pages/projects/Projects.tsx
@@ -15,8 +15,14 @@ const Projects = () => {
         {status === DataStatus.FAILED && <p style={{ color: 'red' }}>{error}</p>}
         {status === DataStatus.SUCCESS && (
           <>
-            <h2>My projects</h2>
             <div className="projects-flex">
+              <div className="projects-flex-top">
+                <h2>My Projects</h2>
+                <h4>
+                  A collection of my recent work. Each project is built with attention to detail and
+                  a commitment to quality.
+                </h4>
+              </div>
               {projects &&
                 projects.map((project) => <ProjectCard key={project._id} project={project} />)}
             </div>
