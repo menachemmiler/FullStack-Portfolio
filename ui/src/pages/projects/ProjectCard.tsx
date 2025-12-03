fb14f31eae8a3c839a62a4113b30e276f4273e73
miler.yesodot <miler.yesodot@gmail.com>
2025-12-03 16:54:30 +0200

    refactor: Simplify ProjectCard component structure and enhance styling; improve hover effects and link visibility

--- ui/src/pages/projects/ProjectCard.tsx
+++ ui/src/pages/projects/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import { Code, ExternalLink, Github, Server } from 'lucide-react';
+import { Code, ExternalLink, Server } from 'lucide-react';
 import { IProject } from '../../utils/interfaces';
 
 interface Props {
@@ -7,56 +7,36 @@ interface Props {
 
 const ProjectCard = ({ project }: Props) => {
   return (
-    <div key={`${project}`} className="project-card">
-      <div className="project-image">
-        <img src={`${project.image}`} alt={`${project.title} img`} />
-      </div>
-      <div className="project-content">
-        <div>
-          <h3>{`${project.title}`}</h3>
-          <p>{`${project.description}`}</p>
-        </div>
+    <div className="project-card">
+      <img className="project-img" src={project.image} alt={project.title} />
 
-        <div className="project-links">
-          <div className="left">
-            <a target="_blank" href={`${project.liveLink}`} className="button">
-              View Project <ExternalLink size={16} />
+      {/* basic info always visible */}
+      <div className="info-bar">
+        <h3>{project.title}</h3>
+        <p>{project.description}</p>
+      </div>
+
+      {/* full panel on hover */}
+      <div className="hover-panel">
+        <h3>{project.title}</h3>
+        <p>{project.fullDescription}</p>
+
+        <div className="links">
+          {project.liveLink && (
+            <a target="_blank" href={project.liveLink} className="button">
+              צפה באתר <ExternalLink size={16} />
             </a>
-            {project.githubAll ? (
-              <a target="_blank" href={`${project.githubAll}`} className="button">
-                View Code <Github size={16} />
-              </a>
-            ) : project.githubClient && project.githubServer ? (
-              <>
-                <a target="_blank" href={`${project.githubClient}`} className="button">
-                  View Code Client <Code size={16} />
-                </a>{' '}
-                <a target="_blank" href={`${project.githubServer}`} className="button">
-                  View Code Server <Server size={16} />
-                </a>
-              </>
-            ) : project.githubServer ? (
-              <a target="_blank" href={`${project.githubServer}`} className="button">
-                View Code Server <Server size={16} />
-              </a>
-            ) : project.githubClient ? (
-              <a target="_blank" href={`${project.githubClient}`} className="button">
-                View Code Client <Code size={16} />
-              </a>
-            ) : (
-              <></>
-            )}
-          </div>
-          <div className="right">
-            <button
-              onClick={() => {
-                alert(`${project.fullDescription}`);
-              }}
-              className="button button-outline"
-            >
-              About
-            </button>
-          </div>
+          )}
+          {project.githubClient && (
+            <a target="_blank" href={project.githubClient} className="button">
+              Client <Code size={16} />
+            </a>
+          )}
+          {project.githubServer && (
+            <a target="_blank" href={project.githubServer} className="button">
+              Server <Server size={16} />
+            </a>
+          )}
         </div>
       </div>
     </div>
