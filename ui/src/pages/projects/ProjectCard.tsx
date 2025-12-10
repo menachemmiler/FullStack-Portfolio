2b5ab0892e7a02145c90a26b5f217e881dded4c5
miler.yesodot <miler.yesodot@gmail.com>
2025-12-10 16:18:06 +0200

    refactor: Simplify ProjectCard component structure and enhance styling; remove unused CSS file

--- ui/src/pages/projects/ProjectCard.tsx
+++ ui/src/pages/projects/ProjectCard.tsx
@@ -5,42 +5,38 @@ interface Props {
   project: IProject;
 }
 
-const ProjectCard = ({ project }: Props) => {
+export default function ProjectCard({ project }: Props) {
   return (
     <div className="project-card">
-      <img className="project-img" src={project.image} alt={project.title} />
+      <img src={project.image} alt={project.title} className="project-img" />
 
-      {/* basic info always visible */}
       <div className="info-bar">
         <h3>{project.title}</h3>
-        <p>{project.description}</p>
-      </div>
+        <p className="short">{project.description}</p>
 
-      {/* full panel on hover */}
-      <div className="hover-panel">
-        <h3>{project.title}</h3>
-        <p>{project.fullDescription}</p>
+        {/* ↓ מה שמופיע רק בהובר (חשוף באנימציה) */}
+        <div className="extra">
+          <p className="full">{project.fullDescription}</p>
 
-        <div className="links">
-          {project.liveLink && (
-            <a target="_blank" href={project.liveLink} className="button">
-              צפה באתר <ExternalLink size={16} />
-            </a>
-          )}
-          {project.githubClient && (
-            <a target="_blank" href={project.githubClient} className="button">
-              Client <Code size={16} />
-            </a>
-          )}
-          {project.githubServer && (
-            <a target="_blank" href={project.githubServer} className="button">
-              Server <Server size={16} />
-            </a>
-          )}
+          <div className="links">
+            {project.liveLink && (
+              <a target="_blank" href={project.liveLink} className="live">
+                צפה באתר <ExternalLink size={16} />
+              </a>
+            )}
+            {project.githubClient && (
+              <a target="_blank" href={project.githubClient}>
+                Client <Code size={16} />
+              </a>
+            )}
+            {project.githubServer && (
+              <a target="_blank" href={project.githubServer}>
+                Server <Server size={16} />
+              </a>
+            )}
+          </div>
         </div>
       </div>
     </div>
   );
-};
-
-export default ProjectCard;
+}
