cb7e0a49fc72685a3ee250c330bf9b1216fac64e
miler.yesodot <miler.yesodot@gmail.com>
2025-12-31 14:06:00 +0200

    CORS_ORIGIN in server to render client

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,16 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import usersController from "./routes/users";
-import productsController from "./routes/projects";
-import { connectToMongo } from "./config/db";
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+import { connectToMongo } from './config/db';
+import productsController from './routes/projects';
+import usersController from './routes/users';
 
 const PORT = process.env.PORT || 3000;
+const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3050';
 
-import http from "http";
-import { Server } from "socket.io";
+import http from 'http';
+import { Server } from 'socket.io';
+import './socket/io'; //מייבא את הקובץ של הסוקטים
 
 const app = express();
 connectToMongo();
@@ -17,23 +19,22 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3050", // כתובת הלקוח
-    methods: "*",
+    origin: CORS_ORIGIN,
+    methods: '*',
   },
 });
-import "./socket/io"; //מייבא את הקובץ של הסוקטים
 
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3050",
+    origin: 'http://localhost:3050',
     credentials: true,
-  })
+  }),
 );
 
-app.use("/api/users", usersController);
+app.use('/api/users', usersController);
 
-app.use("/api/projects", productsController);
+app.use('/api/projects', productsController);
 
 server.listen(PORT, () => {
   console.log(`Server started, Visit "http://localhost:${PORT}"`);
