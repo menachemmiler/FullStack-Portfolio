4d8b3c976d4cf2303ffffae4ced11c58a62d74db
menachem <MS0556727820@GMAIL.COM>
2026-08-26 13:58:20 +0300

    with new db

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -2,11 +2,11 @@ import "dotenv/config";
 
 // אובייקט הגדרות מרכזי
 export const config = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 4000,
   corsOrigin: process.env.CORS_ORIGIN || "http://localhost:3050",
   db: {
     uri:
       process.env.DB_URI ||
-      "mongodb+srv://ms0556727820:gssBTd8i%40j3Gmgc@cluster0.1srxzax.mongodb.net", // ברירת מחדל לפיתוח
+      "mongodb+srv://ms0556727820_db_user:35jfWOEz7VJxcnwW@cluster0.a62e1qq.mongodb.net", // ברירת מחדל לפיתוח
   },
 };
